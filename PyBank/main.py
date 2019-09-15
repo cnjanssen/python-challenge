ec178e2eed2866f2100e49171fd0de519228dc33
cnjanssen <43945514+cnjanssen@users.noreply.github.com>
2019-09-15 16:02:51 -0700

    Add files via upload
    Pushed from other folders...

---
+++ PyBank/main.py
@@ -0,0 +1,86 @@
+import os
+import csv
+
+# path to file to be read
+# Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+# The total number of months included in the dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The average of the changes in "Profit/Losses" over the entire period
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over the entire period
+csv_path = os.path.join("..", "Resources", "budget_data.csv")
+
+
+# The total number of months included in the dataset
+total_months = 0
+net_amt = 0 
+amounts = []
+months = []
+
+with open(csv_path, newline="") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    next(csv_reader)
+
+    for row in csv_reader:
+        # print(f" {row[0]} and value is {row[1]}")
+        total_months +=1
+        #print(f" At second column we have {row[1]}")
+        net_amt = net_amt + int(row[1])
+        months.append(row[0])
+        amounts.append(row[1])
+
+average = net_amt/total_months
+
+#get greatest increase/decrease from list of all amounts
+great_inc = max(amounts)
+great_dec = min(amounts)
+
+#get greatest increase/decrease INDEX from list of all amounts
+index_great_inc = amounts.index(great_inc)
+index_great_dec = amounts.index(great_dec)
+
+
+
+#great_inc_month = months[]
+print (f"There are {total_months} months")
+print (f"There was {net_amt} Dollars  NET")
+print (f"The AVERAGE net change was {average} Dollars AVERAGE PER Month")
+print (f"The greast increase was {great_inc} and it happend at {months[index_great_inc]}  ")
+print (f"The greast decrease was {great_dec} and it happend at {months[index_great_dec]} ")
+
+big_month = str(great_inc+ " " +months[index_great_inc])
+small_month = str(great_dec+ " " +months[index_great_dec])
+
+
+#Writing output to CSV
+
+
+# Specify the file to write to
+output_path = os.path.join(".", ".", "new.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the first row 
+    csvwriter.writerow(['Total Months', total_months])
+
+    # Write the second row
+    csvwriter.writerow(['NET Dollars', net_amt])
+
+     # Write the third row
+    csvwriter.writerow(['AVERAGE NET Dollars', average])
+
+    # Write the fourth row
+    csvwriter.writerow(['GREATEST INCREASE', big_month])
+
+    # Write the fourth row
+    csvwriter.writerow(['GREATEST DECREASE', small_month])
---
+++ PyPoll/main.py
@@ -0,0 +1,95 @@
+import os
+import csv
+
+# path to file to be read
+cereal_csv_path = os.path.join(".", "Resources", "election_data.csv")
+#total number of votes
+total = 0
+#variables for the count of each one
+count_khan = 0 
+count_correy = 0 
+count_li = 0
+count_oTooley = 0 
+
+with open(cereal_csv_path, newline="") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    next(csv_reader)
+
+    # Read through each row of data after the header
+    for row in csv_reader:
+
+        # Convert row to float and compare to grams of fiber
+        #if float(row[7]) >= 5:
+        total +=1
+
+        if (row[2] == 'Khan'):
+            count_khan +=1
+
+        if (row[2] == 'Correy'):
+            count_correy +=1
+
+        if (row[2] == 'Li'):
+            count_li +=1
+
+        if (row[2] == "O'Tooley"):
+            count_oTooley +=1
+
+
+def print_results():
+    print("Election Results:")
+    print (f"total number of votes is {str(total)}")
+    print("-----------------------------------------")
+    print(f"Khan had {total_k} % and {count_khan} votes")
+    print(f"Correy had {total_c} % and {count_correy} votes")
+    print(f"Li had {total_l} % and {count_li} votes")
+    print(f"O'Tooley had {total_o} % and {count_oTooley} votes")
+    print("AND THE WINNER IS......................")
+
+
+
+total_k = 100 * 1/(total / count_khan) 
+total_c = 100 * 1/(total / count_correy)
+total_l = 100 * 1/(total / count_li)
+total_o = 100 * 1/(total / count_oTooley)
+
+percents = [total_k, total_c, total_l, total_o]
+#candidates = ["Khan", ]
+can_per = {total_k: "Khan" , total_c: "Correy", total_l  : "Li",  total_o :"O'Tooley" } 
+
+pop_win = max(percents)
+
+winner = can_per[pop_win]
+
+
+
+print(f"BEGIN")
+print_results()
+print (f"The candidate with the most popular vote had {pop_win} PERCENT and is {winner}")
+print(f"END")
+
+# Specify the file to write to
+output_path = os.path.join(".", ".", "results.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the first row 
+    csvwriter.writerow(['Candidate', 'NUMBER', 'Percentages'])
+
+    #write all necessary candidate percentages
+    for cand in can_per:
+        print(cand)
+        csvwriter.writerow([can_per[cand], cand * total, cand])
+        #print (f"The candidate with the most popular vote had {pop_win} PERCENT and is {winner}")
+        #csvwriter.writerow([cand, int(can_per[cand]) * total, int(can_per[cand]])
+        
+    #write line break
+    csvwriter.writerow(["-----", "------", "------"])
+    
+    #write winner row(last ROW)
+    win_str = str(pop_win) + " %"
+    csvwriter.writerow(["The WINNNER", win_str, winner])
